7e47a34ae4851ebe93e2ea2b7c81335dea235e91
YanethM <yaneth.mejiar@autonoma.edu.co>
2022-05-17 09:40:50 -0500

    Admin scss

--- src/pages/Admin/SignIn/SignIn.js
+++ src/pages/Admin/SignIn/SignIn.js
@@ -2,21 +2,28 @@ import React from "react";
 import { Layout, Tabs } from "antd";
 import Login from "../../../components/AdminComponents/Login";
 import Register from "../../../components/AdminComponents/Register";
+import Logo from "../../../assets/img/png/Logo2.png";
+import "./SigIn.scss";
 
 export default function SignIn() {
   const { Content } = Layout;
   const { TabPane } = Tabs;
   return (
-    <Layout>
-      <Content>
-        <Tabs type="card">
-          <TabPane tab={<span>Iniciar sesión</span>} key="1">
-            <Login />
-          </TabPane>
-          <TabPane tab={<span>Registro</span>} key="2">
-            <Register />
-          </TabPane>
-        </Tabs>
+    <Layout className="sign-in">
+      <Content className="sign-in__content">
+        <h1 className="sign-in__content-logo">
+          <img src={Logo} alt="Alternate"/>
+        </h1>
+        <div className="sign-in__content-tabs">
+          <Tabs type="card">
+            <TabPane tab={<span>Iniciar sesión</span>} key="1">
+              <Login />
+            </TabPane>
+            <TabPane tab={<span>Registro</span>} key="2">
+              <Register />
+            </TabPane>
+          </Tabs>
+        </div>
       </Content>
     </Layout>
   );
