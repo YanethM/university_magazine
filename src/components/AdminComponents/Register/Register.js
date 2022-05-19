78e08d22661a5126d1340c0ccd53d1ad46d8edcb
YanethM <yaneth.mejiar@autonoma.edu.co>
2022-05-19 05:13:52 -0500

    Without validations

--- src/components/AdminComponents/Register/Register.js
+++ src/components/AdminComponents/Register/Register.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Form, Input, Button, Checkbox } from "antd";
+import { Form, Input, Button, Checkbox, notification } from "antd";
 import { UserOutlined, LockOutlined } from "@ant-design/icons";
 import {
   emailValidation,
@@ -7,17 +7,13 @@ import {
 } from "../../../validations/FormValidations";
 import "./Register.scss";
 
-export default function Register() {
-  /* Validar valor - estado inicial de los campos input vacíos */
+export default function RegisterForm() {
   const [inputs, setInputs] = useState({
-    person_name: "",
-    lastname: "",
     email: "",
     password: "",
     repeatPassword: "",
     privacyPolicy: false,
   });
-
   const [formValid, setFormValid] = useState({
     email: false,
     password: false,
@@ -25,114 +21,22 @@ export default function Register() {
     privacyPolicy: false,
   });
 
-  const changeForm = (event) => {
-    if (event.target.name === "privacyPolicy") {
+  const changeForm = (e) => {
+    if (e.target.name === "privacyPolicy") {
       setInputs({
         ...inputs,
-        [event.target.name]: event.target.checked,
+        [e.target.name]: e.target.checked,
       });
     } else {
       setInputs({
         ...inputs,
-        [event.target.name]: event.target.value,
+        [e.target.name]: e.target.value,
       });
     }
-  };
-
-  const inputValidation = (e) => {
-    console.log("Validando");
-    const { type, name } = e.target;
-
-    if (type === "email") {
-      setFormValid({ ...formValid, [name]: emailValidation(e.target) });
-    }
-    if (type === "password") {
-      setFormValid({ ...formValid, [name]: minLengthValidation(e.target, 6) });
-    }
-    if (type === "checkbox") {
-      setFormValid({ ...formValid, [name]: e.target.checked });
-    }
-  };
-
-  const register = async (e) => {
-    e.preventDefault();
-    console.log(formValid);
-  };
-  /*const emailVal = inputs.email;
-    const passwordVal = inputs.password;
-    const repeatPasswordVal = inputs.repeatPassword;
-    const privacyPolicyVal = inputs.privacyPolicy;
-
-    if (!emailVal || !passwordVal || !repeatPasswordVal || !privacyPolicyVal) {
-      notification["error"]({
-        message: "Todos los campos son obligatorios",
-      });
-    } else {
-      if (passwordVal !== repeatPasswordVal) {
-        notification["error"]({
-          message: "Las contraseñas tienen que ser iguales.",
-        });
-      } else {
-        const result = await signUpApi(inputs);
-        if (!result.ok) {
-          notification["error"]({
-            message: result.message,
-          });
-        } else {
-          notification["success"]({
-            message: result.message,
-          });
-          resetForm();
-        }
-      }
-    }
-  };
- */
-  const resetForm = () => {
-    const inputs = document.getElementsByTagName("input");
-
-    for (let i = 0; i < inputs.length; i++) {
-      inputs[i].classList.remove("success");
-      inputs[i].classList.remove("error");
-    }
-
-    setInputs({
-      email: "",
-      password: "",
-      repeatPassword: "",
-      privacyPolicy: false,
-    });
-
-    setFormValid({
-      email: false,
-      password: false,
-      repeatPassword: false,
-      privacyPolicy: false,
-    });
   };
 
   return (
-    <Form className="register-form" onChange={changeForm}>
-      <Form.Item>
-        <Input
-          prefix={<UserOutlined className="site-form-item-icon" />}
-          type="text"
-          name="person_name"
-          placeholder="Nombre"
-          className="register-form__input"
-          value={inputs.person_name}
-        />
-      </Form.Item>
-      <Form.Item>
-        <Input
-          prefix={<UserOutlined className="site-form-item-icon" />}
-          type="text"
-          name="lastname"
-          placeholder="Apellido"
-          className="register-form__input"
-          value={inputs.lastname}
-        />
-      </Form.Item>
+    <Form className="register-form" onSubmit={register} onChange={changeForm}>
       <Form.Item>
         <Input
           prefix={<UserOutlined className="site-form-item-icon" />}
